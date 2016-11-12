ed5af9e7fe6cd06a5b198c50b2bf8bf71ac2be8d
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2016-11-12 13:01:54 +0200

    RLE functions accept sequences and return arays. RLE.decode will not fail in case of negative numbers.

--- src/bsc/RLE.fs
+++ src/bsc/RLE.fs
@@ -1,33 +1,37 @@
+// Copyright (c) 2016 Theodore Tsirpanis
+// 
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+
 namespace Brainsharp
 
 /// <summary>Run-length Encoding functions.<summary/>
 module RLE = 
     /// <summary>
-    /// Encodes a list of objects using Run-length Encoding.
+    /// Encodes a sequence of objects using Run-length Encoding.
     /// </summary>
-    /// <param name="l">The list of objects.</param>
+    /// <param name="l">The sequence of objects.</param>
     /// <returns>
     /// A tuple of an integer and an object.
     /// For example, <c>encode [7;7;7]<c/>, will return <c>(3, 7)<c/>.
     /// <returns/>
     let encode l = 
-        let rec pack (l, b : (int * 'a) list) = 
-            match l with
-            | [] -> b
-            | _ -> 
-                let head = List.head l
+        let rec pack l b = 
+            if Seq.isEmpty l then
+                b
+            else  
+                let head = Seq.head l
                 
                 let count = 
                     l
-                    |> List.takeWhile (fun t -> t = head)
-                    |> List.length
+                    |> Seq.takeWhile (fun t -> t = head)
+                    |> Seq.length
                 
-                let newb = b @ [ (count, head) ]
-                pack (l |> List.skip count, newb)
-        pack (l, [])
-
+                let newb = Seq.append b [(count,head)]
+                pack (Seq.skip count l) newb
+        pack l [] |> Array.ofSeq
+    
     /// <summary>
     /// Decodes a pair of integers and objects returned by <c>encode<c/>.
     /// </summary>
-    /// <param name="l"></param>
-    let decode l = l |> List.collect (fun (count, item) -> List.replicate count item)
+    let decode l = l |> Seq.filter (fun i -> fst i > 0) |> Seq.collect (fun (count, item) -> Seq.replicate count item) |> Array.ofSeq
