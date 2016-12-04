5e8c5b03ad3627bf443b09c24995c17f8f23cc59
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2016-12-04 14:40:51 +0200

    Added code tree types and functions.

---
+++ src/bsc/BFCode.fs
@@ -0,0 +1,29 @@
+// Copyright (c) 2016 Theodore Tsirpanis
+// 
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+namespace Brainsharp
+
+type BFCodeTree = BFToken list
+
+and BFToken = 
+    | MemoryControl of byte
+    | PointerControl of int
+    | IOWrite
+    | IORead
+    | Loop of BFCodeTree
+
+module BFCode = 
+    open System
+    open BFParser
+    
+    let rec makeCodeTree s = 
+        s
+        |> List.map (function
+               |Plus -> MemoryControl 1uy
+               |Minus -> MemoryControl 255uy
+               |Left -> PointerControl Int32.MaxValue
+               |Right -> PointerControl 1
+               |Dot -> IOWrite
+               |Comma -> IORead
+               |BracketLoop body -> Loop (makeCodeTree body))
